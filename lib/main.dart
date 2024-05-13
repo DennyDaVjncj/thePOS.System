3266a5f1b56e8784f6ec9a2bdefcb09343d9f8f3
DennyDaVjncj <daVjncjCode@gmail.com>
2024-05-13 09:44:55 -0400

    updated homePage-class property

--- lib/main.dart
+++ lib/main.dart
@@ -71,7 +71,9 @@ class _MyHomePageState extends State<MyHomePage> {
               ],
               selectedIndex: 0,
               onDestinationSelected: (value) {
-                print('selected: $value');
+                setState(() {
+                  selectedIndex = value;
+                });
               },
             ),
           ),
